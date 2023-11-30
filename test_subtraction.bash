5c75aeff42b3f047500ece79c2bba681c41352dd
Nishio Chihiro <chi16240@gmail.com>
2023-11-30 18:39:21 +0900

    add a comand

---
+++ test_subtraction.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Nishio Chihiro
+# SPDX-License-Identifier: BSD-3-clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+out=$(seq 5 | ./subtraction)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+
+[ "$res" = 0 ] && echo OK
+exit $res
