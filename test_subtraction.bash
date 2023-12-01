c45d6e81132172cf5e1781b2c0714190c85aeda7
Nishio Chihiro <chi16240@gmail.com>
2023-12-01 11:45:03 +0900

    add a comand

--- test_subtraction.bash
+++ test_subtraction.bash
@@ -3,15 +3,15 @@
 # SPDX-License-Identifier: BSD-3-clause
 
 ng () {
-	echo NG at Line $0
+	echo NG at Line $1
 	res=1
 }
 
 res=0
 
 
-out=$(seq 8 | ./subtraction)
-[ "${out}" = 15 ] || ng ${LINENO}
+out=$(seq 1 | ./subtraction)
+[ "${out}" = -1 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
